b8921bc3fba734bf6ebb3fe440c2da7d35354c42
reigamogus <matheusbarneygalindo@gmail.com>
2025-08-06 18:07:13 -0300

    Uso de REF e DEREF

--- AV6/Script_Consultas_OR.sql
+++ AV6/Script_Consultas_OR.sql
@@ -1,2 +1,7 @@
 -- SCRIPT PARA CONSULTAS OR
 
+--  Selecionar referências dos bombeiros e seus respectivos quartéis
+--  Utiliza a função DEREF para acessar os atributos do objeto referenciado
+SELECT p.nome AS nome_bombeiro, DEREF(TREAT(VALUE(p) AS tp_bombeiro).quartel_ref).nome AS nome_quartel
+FROM tb_pessoas p
+WHERE VALUE(p) IS OF (tp_bombeiro)
