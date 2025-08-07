38dc681632ac8d4fbc74bca6d105c1695c334487
reigamogus <matheusbarneygalindo@gmail.com>
2025-08-07 11:06:48 -0300

    Add bombeiro-quartel queries with REF and DEREF
    Atualizei minha consulta de DEREF e adicionei uma consulta que pega diretamente os OID da REF feita para quarteis
    
    Também explicitei porque no nosso projeto é necessário usar TREAT (não dado na monitoria) e msmo assim dou um exemplo que realmente usa SELECT REF

--- AV6/Script_Consultas_OR.sql
+++ AV6/Script_Consultas_OR.sql
@@ -1,7 +1,24 @@
 -- SCRIPT PARA CONSULTAS OR
 
---  Selecionar referências dos bombeiros e seus respectivos quartéis
---  Utiliza a função DEREF para acessar os atributos do objeto referenciado
-SELECT p.nome AS nome_bombeiro, DEREF(TREAT(VALUE(p) AS tp_bombeiro).quartel_ref).nome AS nome_quartel
+--  Como usamos uma organização onde temos uma tabela de pessoas que é supertipo, em vez de ter várias
+-- tabelas para cada subtipo, usamos de TREAT para acessar a referência do quartel a partir da tabela de pessoas.
+
+-- Retornar OID do quartel associado a cada bombeiro:
+SELECT p.nome AS nome_bombeiro,
+       REFTOHEX(TREAT(VALUE(p) AS tp_bombeiro).quartel_ref) AS referencia_quartel
 FROM tb_pessoas p
-WHERE VALUE(p) IS OF (tp_bombeiro)
+WHERE VALUE(p) IS OF (tp_bombeiro);
+/*
+Só para deixar claro o conhecimento, o uso direto de REF com um tb_bombeiros seria algo tipo:
+SELECT b.nome AS nome_bombeiro,
+       REFTOHEX(REF(b)) AS referencia_bombeiro
+FROM tb_bombeiro b;
+*/
+
+-- Retornar o nome do quartel associado a cada bombeiro:
+SELECT p.nome AS nome_bombeiro, 
+    DEREF(TREAT(VALUE(p) AS tp_bombeiro).quartel_ref).cnpj AS cnpj_quartel, 
+    DEREF(TREAT(VALUE(p) AS tp_bombeiro).quartel_ref).nome AS nome_quartel, 
+    DEREF(TREAT(VALUE(p) AS tp_bombeiro).quartel_ref).endereco AS endereco_quartel
+FROM tb_pessoas p
+WHERE VALUE(p) IS OF (tp_bombeiro);
